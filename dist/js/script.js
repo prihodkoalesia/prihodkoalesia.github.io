9062169645d6c4647771cfc9b815ec8639b71c69
wb_prihodko <prihodko@globalsolutions.ru>
2017-07-13 18:01:51 +0300

    добавила блоки баннера, навыков, портфолио и контактов

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,36 @@
 (function ($) {
     $(document).ready(function () {
-		
+        inWindow('.elem_anim');
+        ShowMenu();
+        $(window).scroll(function(){
+            inWindow('.elem_anim');
+            ShowMenu();
+        })
     });
 
-    function () {
-        
+    function inWindow(s){
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var currentEls = $(s);
+        currentEls.each(function(){
+            var el = $(this);
+            var offset = el.offset();
+            if(scrollTop <= offset.top && (el.height() + offset.top) < (scrollTop + windowHeight)){
+                $(this).addClass('go')
+            }
+        });
+    }
+
+    function ShowMenu() {
+
+            var $scroll = $(window).scrollTop();
+            if($scroll > 100){
+                $('header').stop().animate({top: "0px"}, 500);
+                // $('#scrollTop').stop().animate({bottom: "20px"}, 500);
+            }else {
+                $('header').stop().animate({top: "-70px"}, 500);
+                // $('#scrollTop').stop().animate({bottom: "-60px"}, 500);
+            };
     }
 
 
